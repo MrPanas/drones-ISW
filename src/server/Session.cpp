1eda20f239ee37273817b8520cb8f28277003491
FelixRehrl <felix.rehrl@kabelmail.de>
2024-05-17 09:40:13 +0200

    get paths with init times

--- src/server/Session.cpp
+++ src/server/Session.cpp
@@ -1,8 +1,14 @@
 #include "Session.hpp"
+#include <cstddef>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
 #include <iostream>
+#include <list>
 #include <ostream>
 #include <postgresql/libpq-fe.h>
+#include <string>
 
 http_session::http_session(tcp::socket socket) : socket_(std::move(socket)) {}
 
@@ -23,32 +29,82 @@ void http_session::do_read() {
 // Process the HTTP request
 void http_session::process_request() {
   if (request_.method() == http::verb::post && request_.target() == "/report") {
+
+    // TODO:
+    // retrieve    @var cc_id   ;  @var timestamp
+    //
     handle_request_report();
   } else {
     send_not_found();
   }
 }
 
-// Handle specific request to generate a report
+class PathException : public std::exception {
+private:
+  std::string message;
+
+public:
+  PathException(const std::string &msg) : message(msg) {}
+
+  virtual const std::string what() throw() { return message.c_str(); }
+};
+
+struct path_drone_scann_init {
+  int path_id;
+  int drone_id;
+  std::string scan_init_time;
+
+  path_drone_scann_init(int p_id, int d_id, const std::string &time)
+      : path_id(p_id), drone_id(d_id), scan_init_time(time) {}
+};
+
+void get_most_recent_paths(
+    std::list<path_drone_scann_init> &paths_with_init_times) {
+
+  Con2DB db("localhost", "5432", "postgres", "postgres", "postgres");
+  char sqlcmd[512];
+
+  snprintf(sqlcmd, sizeof(sqlcmd),
+           "SELECT path.path_id, dl.drone_id, MAX(dl.time) AS max_time "
+           "FROM path "
+           "LEFT JOIN drone_log AS dl ON path.path_id = dl.path_id AND "
+           "dl.status = 'SCANNING' "
+           "GROUP BY path.path_id, dl.drone_id ORDER BY path.path_id");
+
+  PGresult *res = db.ExecSQLtuples(sqlcmd);
+
+  for (int i = 0; i < PQntuples(res); i++) {
+    int path_id =
+        strtol(PQgetvalue(res, i, PQfnumber(res, "path_id")), NULL, 10);
+    int drone_id =
+        strtol(PQgetvalue(res, i, PQfnumber(res, "drone_id")), NULL, 10);
+    const char *time_scan_str = PQgetvalue(res, i, PQfnumber(res, "max_time"));
+
+    if (time_scan_str == NULL || strlen(time_scan_str) == 0) {
+      throw PathException("Path with id : " + std::to_string(path_id) +
+                          " has not drone");
+    } else {
+      paths_with_init_times.push_back(
+          path_drone_scann_init(path_id, drone_id, std::string(time_scan_str)));
+    }
+  }
+}
+/**
+ * @var int cc_id
+ * @var timestamp map_timestamp
+ */
 void http_session::handle_request_report() {
 
+  std::list<path_drone_scann_init> paths;
+
   try {
-    Con2DB db("localhost", "5432", "postgres", "postgres", "postgres");
-
-    char sqlcmd[256];
-    sprintf(sqlcmd, "SELECT * FROM drone");
-
-    PGresult *res = db.ExecSQLtuples(sqlcmd);
-
-    if (res != nullptr) {
-      int rows = PQntuples(res);
-      std::cout << "We have retrieved " << rows << " rows" << std::endl;
-      PQclear(res); // Clear the result object
-    } else {
-      std::cerr << "Failed to execute the SQL command." << std::endl;
-    }
-  } catch (const std::exception &e) {
-    std::cerr << "Exception: " << e.what() << std::endl;
+    get_most_recent_paths(paths);
+  } catch (PathException &pe) {
+    std::cerr << pe.what() << std::endl;
+  }
+  for (const auto &path : paths) {
+    std::cout << "Path ID: " << path.path_id << ", Drone ID: " << path.drone_id
+              << ", Latest Scan Time: " << path.scan_init_time << std::endl;
   }
 
   response_.version(request_.version());
