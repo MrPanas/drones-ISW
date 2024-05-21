b5b5083c5e46e25c67e08b7fcb7ca7d6a1d82ff7
Patryk Jan Mulica <mulica.1986671@studenti.uniroma1.it>
2024-05-21 19:06:32 +0200

    implemented stop after a given time

--- src/config.cpp
+++ src/config.cpp
@@ -10,5 +10,7 @@ namespace Config {
     int DRONE_STEPS = 15000;
 
     int POINT_EXPIRATION_TIME = 300000;
-    int NUMBER_OF_DRONES = 4000;
+    int NUMBER_OF_DRONES = 9000;
+
+    unsigned int TIME_TO_SCAN = 181; // in minutes
 }
